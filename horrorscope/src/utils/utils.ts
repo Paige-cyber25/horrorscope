12a54cf72bc6f6b6862f115d56ab1c767c5bd0c7
Patience <ukapatience7@gmail.com>
2025-10-22 20:54:12 +0100

    fix: fix errors breaking pipeline

--- horrorscope/src/utils/utils.ts
+++ horrorscope/src/utils/utils.ts
@@ -111,6 +111,10 @@ export type SignupData = {
   updatedAt: string;
 };
 
+export type ErrorResponse = {
+  message: string;
+};
+
 export const capitalizeFirstLetter = (str: string) => {
   if (!str) return "";
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
